454de700a25477d3279a66d915fff00226728cc3
logan1x <sharmakhushal78@gmail.com>
2024-01-07 20:14:09 +0530

    initial commit

---
+++ src/components/PreviewImage.js
@@ -0,0 +1,620 @@
+import React from "react";
+
+// ignore ts errors in whole file
+// @ts-ignore
+
+const LongWave = () => (
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 754 93"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.65515 19.106V76.9026"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M15.6301 10.0522V84.561"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M23.2919 41.3879V51.8355"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M30.2404 12.141V81.7754"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M37.9153 5.87448V89.4354"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M44.8638 34.4246V58.1019"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M52.5322 37.9054V55.3172"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M59.5005 44.8688V49.7451"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M65.7554 37.9054V55.3172"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M73.4304 34.4246V58.1019"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M80.3789 28.8549V65.763"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M88.7474 17.0155V77.5974"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M95.6959 33.0342V60.8894"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M102.677 35.1214V59.4951"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M109.619 28.8549V65.0666"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M117.294 33.7261V60.8849"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M124.949 37.9054V57.4044"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M131.898 40.6911V53.9241"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M138.886 43.4784V50.4438"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M146.528 46.2624V48.3523"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M153.49 40.6911V53.9241"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M160.458 24.6773V67.8506"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M168.12 33.0342V61.5858"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M175.775 19.8028V74.814"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M183.443 15.6218V78.9892"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M190.412 10.7506V83.1705"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M197.367 25.3724V69.2421"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M205.042 31.6389V62.976"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M211.99 39.2991V53.925"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M218.945 12.8362V79.6852"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M226.62 28.1581V65.7607"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M234.282 12.8362V79.6852"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M241.23 37.9054V56.0137"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M248.892 41.3879V51.139"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M255.867 33.0342V59.4966"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M263.509 40.6911V51.8352"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M270.484 42.7816V49.747"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M276.059 44.8688V48.3518"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M283.014 37.9054V54.6208"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M291.369 34.4246V59.4942"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M298.324 25.3724V68.5458"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M304.599 5.87448V88.0428"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M311.574 19.8028V72.725"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M319.235 29.5518V64.3707"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M326.884 44.8688V46.9586"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M333.872 42.7816V49.747"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M341.514 35.8149V56.0103"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M348.469 42.0831V50.4408"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M355.438 33.0342V57.4079"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M363.106 30.9421V60.8864"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M370.755 39.9943V52.5308"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M377.716 29.5518V65.0671"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M384.684 34.4246V58.1019"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M392.34 38.6023V55.3176"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M399.995 40.6911V51.1387"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M406.97 42.7816V49.0505"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M413.925 44.1719V47.655"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M421.587 37.2086V54.6204"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M428.562 37.2086V54.6204"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M436.21 30.2469V61.584"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M443.178 20.4963V67.8477"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M450.127 1V92.2207"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M458.495 11.4458V81.7766"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M464.763 20.4963V71.3295"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M472.425 25.3724V68.5458"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M479.374 28.8549V63.6739"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M487.049 33.0342V60.8894"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M494.01 35.1214V55.3168"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M501.666 39.9943V53.9237"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M508.641 42.7816V49.747"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M515.582 44.8688V47.6552"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M523.251 39.9943V53.2272"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M530.926 39.9943V53.9237"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M537.881 33.7261V58.0998"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M544.823 1.69519V90.1306"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M552.511 9.35696V81.7768"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M559.466 15.6218V76.2038"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M567.121 22.5851V69.2402"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M574.083 35.8149V57.4031"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M581.751 21.1948V69.9389"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <path
+      d="M1 33.7261V60.8849"
+      stroke="white"
+      strokeOpacity="0.7"
+      strokeWidth="1.1083"
+      strokeLinecap="round"
+    />
+    <line x1="589" y1="46.5" x2="753.012" y2="46.5" stroke="#F9F9F9" />
+  </svg>
+);
+
+export const PreviewImage = ({ name }) => {
+  return (
+    <div
+      style={{
+        fontSize: 12,
+        background: "black",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        padding: 70,
+      }}
+    >
+      <h1 style={{ color: "#f2f2f2", fontSize: 40 }}>Dynamic Image example</h1>
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1, display: "flex" }}>
+          <LongWave />
+        </div>
+        <div
+          style={{
+            backgroundColor: "white",
+            display: "flex",
+            alignItems: "center",
+            gap: 10,
+            borderRadius: 10000,
+            padding: "10px 13px",
+            width: "auto",
+          }}
+        >
+          <span style={{ fontSize: 24 }}>{name}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
