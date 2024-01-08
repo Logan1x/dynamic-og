2c4b07df3ef32e58e07b1c2ad1ab7e696d564d88
logan1x <sharmakhushal78@gmail.com>
2024-01-08 14:29:47 +0530

    rollback to solve params issue.

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta property="og:image" content={`${process.env.BASE_URL}/api/og`} />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/shop/[name]/page.jsx
+++ src/app/shop/[name]/page.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Head from "next/head";
 
-function Page() {
+function Page({ params }) {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <head>
+        <title>Test page</title>
+        <meta
+          property="og:image"
+          content={`${process.env.BASE_URL}/api/og?name=${params.name}`}
+        />
+      </head>
+      <h1>Shop</h1>
+      <p>Here is a list of products</p>
+    </>
   );
 }
 
